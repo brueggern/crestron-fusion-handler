2b5b303dafd65e86bf65fcfc84151556bd3f82e1
Nicolas Bruegger <brueggern@gmail.com>
2019-04-03 23:01:12 +0200

    stub for the CrestronFusionHandler

--- src/CrestronFusionHandler.php
+++ src/CrestronFusionHandler.php
@@ -2,7 +2,76 @@
 
 namespace Brueggern\CrestronFusionHandler;
 
+use DateTime;
+use Brueggern\CrestronFusionHandler\Collection;
+use Brueggern\CrestronFusionHandler\Entities\Room;
+use Brueggern\CrestronFusionHandler\Exceptions\CrestronFusionException;
+
 class CrestronFusionHandler
 {
+    /**
+     * Client for the Crestron Fusion API
+     *
+     * @var CrestronFusionClient
+     */
+    protected $client;
 
+    /**
+     * Auth token and user must be specified
+     *
+     * @param string $authToken
+     * @param string $authUser
+     */
+    public function __construct(string $authToken, string $authUser)
+    {
+        $this->client = new CrestronFusionClient($authToken, $authUser);
+    }
+
+    /**
+     * Get all rooms
+     *
+     * @return Collection
+     */
+    public function getRooms() : Collection
+    {
+        try {
+            
+        }
+        catch (CrestronFusionException $e) {
+
+        }
+    }
+
+    /**
+     * Get all appointments of a specific day
+     *
+     * @param DateTime $dateTime
+     * @return Collection
+     */
+    public function getAppointments(DateTime $dateTime) : Collection
+    {
+        try {
+            
+        }
+        catch (CrestronFusionException $e) {
+
+        }
+    }
+
+    /**
+     * Get all appointments of a specific room and day 
+     *
+     * @param Room $room
+     * @param DateTime $dateTime
+     * @return Collection
+     */
+    public function getAppointmentsByRoom(Room $room, DateTime $dateTime) : Collection
+    {
+        try {
+            
+        }
+        catch (CrestronFusionException $e) {
+
+        }
+    }
 }
